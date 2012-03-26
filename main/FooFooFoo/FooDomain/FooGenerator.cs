9a15d41b208c78046baab4e8626e48d0fcf4a6f9
[davidsutcliffe] <[david.sutcliffe@lemansholdings.co.uk]>
2012-03-26 16:59:51 +0100

    add stuff for testing

---
+++ main/FooFooFoo/FooDomain/FooGenerator.cs
@@ -0,0 +1,20 @@
+﻿namespace FooDomain
+{
+    public class FooGenerator
+    {
+        public string GetFoo(int fooId)
+        {
+            switch (fooId)
+            {
+                case 1:
+                    return "a";
+                case 2:
+                    return "b"; 
+                case 3:
+                    return "c";
+                default:
+                    return "z";
+            }
+        }
+    }
+}
---
+++ main/FooFooFoo/FooDomainTestt/FooGeneratorTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FooDomain;
+using NUnit;
+using NUnit.Framework;
+
+namespace FooDomainTest
+{
+    [TestFixture]
+    public class FooGeneratorTest
+    {
+        [Test]
+        public void GeneratingFooForKnownFooIdGetsCorrectFoo()
+        {
+            var foo = new FooGenerator();
+            Assert.That(foo.GetFoo(1), Is.EqualTo("a"));
+            Assert.That(foo.GetFoo(2), Is.EqualTo("b"));
+            Assert.That(foo.GetFoo(3), Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void GeneratingFooForUnknownFooIdGetsCorrectFoo()
+        {
+            var foo = new FooGenerator();
+            Assert.That(foo.GetFoo(4), Is.EqualTo("z"));
+            Assert.That(foo.GetFoo(999), Is.EqualTo("z"));
+        }
+    }
+}
